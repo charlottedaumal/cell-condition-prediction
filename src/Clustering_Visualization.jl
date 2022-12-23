fd11f252b2ce8fe0136a594cf14aa44fee5f5b5c
charlottedaumal <92551313+charlottedaumal@users.noreply.github.com>
2022-12-23 12:09:28 +0100

    Update Clustering_Visualization.jl

--- src/Clustering_Visualization.jl
+++ src/Clustering_Visualization.jl
@@ -1,1 +1,27 @@
+#loading useful packages
 using Pkg
+Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
+using CSV, DataFrames, MLJ, MLJLinearModels, Random, Distributions, MLJMultivariateStatsInterface, Plots, MLJClusteringInterface, Distances
+
+train_data = CSV.read(joinpath(@__DIR__, "data", "train.csv"), DataFrame); #loading the .CSV file containing the training data
+
+dropmissing!(train_data); #removing rows with missing values
+
+coerce!(train_data, :labels => Multiclass); #changing the type of the labels column to Multiclass
+
+all_train_data_output = train_data.labels; #DataFrame containing the labels of the training data
+all_train_data_input = select(train_data, Not(:labels)); #DataFrame containing all training data except labels
+
+standard_deviation_columns = std.(eachcol(all_train_data_input)); #computing the standard deviation of each column of the training data
+const_columns_indices = [i for i in 1:size(all_train_data_input)[2] if standard_deviation_columns[i]==0]; #storing the indices of the columns for which the standard deviation is equal to 0
+all_clean_const_train_input = select(all_train_data_input, Not(const_columns_indices)); #keeping only the columns for which the standard deviation is larger than 0
+
+correlated_columns_indices = findall(≈(1), cor(Matrix(all_clean_const_train_input))) |> idxs -> filter(x -> x[1] > x[2], idxs); #storing all the indices of the correlated columns in the DataFrame
+correlated_columns_indices = sort(union(idx.I[1] for idx in correlated_columns_indices)); #sorting the correlated columns' indices and keeping only one index to avoid pairs and removing to many columns
+all_clean_uncorrelated_train_input = select(all_clean_const_train_input, Not(correlated_columns_indices)) #keeping only one column of the pairs of correlated columns in the DataFrame
+
+mdenoise = fit!(machine(PCA(maxoutdim = 2), all_clean_uncorrelated_train_input), verbosity = 0); #fitting a PCA machine for denoising the training data
+report(mdenoise) #reporting the main characteristics of the PCA denoising machine
+cleaned_train_input_PCA_visualization = MLJ.transform(mdenoise, all_clean_uncorrelated_train_input) #transforming our training dataset into a 5000X2 DataFrame (only keep 2 predictors/genes for the clustering)
+
+clustering_visualization = scatter(cleaned_train_input_PCA_visualization[:,1], cleaned_train_input_PCA_visualization[:,2], legend = false, c = Int.(int(all_train_data_output)), title = "Clustering Visualization", xlabel = "Gene 1", ylabel = "Gene 2") #plotting the clusters of the training data
